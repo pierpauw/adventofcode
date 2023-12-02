b11f4867c2a6a199d63dbc3501239478faaba838
Pierre <pierpauw@gmail.com>
2023-12-02 18:49:09 +0100

    d02b Pierre done

--- 2023/Pierre/d02b.py
+++ 2023/Pierre/d02b.py
@@ -3,8 +3,6 @@ import re
 PATH = "C:\\Users\\pierp\\Documents\\Code\\adventofcode\\2023\\Pierre\\"
 input = open(PATH+"d02a_input.txt", "r")
 
-RED, GREEN, BLUE = 12, 13, 14
-
 sum = 0
 
 def parseNumber(regex, string):
@@ -18,19 +16,18 @@ for line in input.readlines():
     game = int(re.search(r"Game (\d*):", line).group()[5:-1])
     hands = re.findall(r"[\w|\s|,]*[;|red$|green$|blue$]", line)
 
-    validGame = True
+    minRed, minGreen, minBlue = 0, 0, 0
     for hand in hands:
         r = parseNumber(r"(?P<number>\d*) red", hand)
         g = parseNumber(r"(?P<number>\d*) green", hand)
         b = parseNumber(r"(?P<number>\d*) blue", hand)
-        if (r>RED) or (g>GREEN) or (b>BLUE):
-            validGame = False
-            break
-    
-    if validGame:
-        print(game, "valid")
-        sum += game
-    else:
-        print(game, "not valid")
+        if (r>minRed):
+            minRed = r
+        if (g>minGreen):
+            minGreen = g
+        if (b>minBlue):
+            minBlue = b
+
+    sum += minRed * minGreen * minBlue
 
 print(sum)
