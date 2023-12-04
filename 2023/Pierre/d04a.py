63c5b46728b96b6cc88ebb706507e466450ca5c3
Pierre <pierpauw@gmail.com>
2023-12-05 00:52:38 +0100

    d04a Pierre done (easy peasy lemon squeezy)

---
+++ 2023/Pierre/d04a.py
@@ -0,0 +1,23 @@
+import re
+
+PATH = "C:\\Users\\pierp\\Documents\\Code\\adventofcode\\2023\\Pierre\\"
+input = open(PATH+"d04a_input.txt", "r")
+
+sum = 0
+
+for line in input.readlines():
+    # print()
+    winning = line[10:39].split()
+    scratched = line[42:-1].split()
+    gain = 0
+    for n in scratched:
+        if n in winning:
+            # print(n)
+            if gain==0:
+                gain = 1
+            else:
+                gain *= 2
+    sum += gain
+
+print(sum)
+    
