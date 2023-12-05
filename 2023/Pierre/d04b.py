cddb43a27c1d4641ff0c43472e61ff2863a015ad
Pierre <pierpauw@gmail.com>
2023-12-05 01:15:28 +0100

    d04b Pierre done (honnetement c'est tranquille)

---
+++ 2023/Pierre/d04b.py
@@ -0,0 +1,27 @@
+import re
+
+PATH = "C:\\Users\\pierp\\Documents\\Code\\adventofcode\\2023\\Pierre\\"
+input = open(PATH+"d04a_input.txt", "r")
+
+NUMCARDS = 213
+
+# card num = index
+gains = [0 for _ in range(NUMCARDS+1)]
+quantities = [1 for _ in range(NUMCARDS+1)]
+quantities[0] = 0 
+
+cardNum = 0
+for line in input.readlines():
+    cardNum += 1
+    winning = line[10:39].split()
+    scratched = line[42:-1].split()
+    
+    for n in scratched:
+        if n in winning:
+            gains[cardNum]+=1
+    
+    for i in range(cardNum+1, cardNum+1+gains[cardNum]):
+        # create as much copies as you have this card (original & copies)
+        quantities[i] += quantities[cardNum]
+
+print(sum(quantities))
